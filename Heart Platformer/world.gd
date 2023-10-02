0ae4264c447619e5b31fdb457bd45f264039bc95
qwe321qwe321qwe321 <aegis321123@gmail.com>
2023-10-03 04:33:54 +0800

    add tilemap and level transition

--- Heart Platformer/world.gd
+++ Heart Platformer/world.gd
@@ -1,15 +1,22 @@
 extends Node2D
 
-
-@onready var collision_polygon_2d: CollisionPolygon2D = $StaticBody2D/CollisionPolygon2D
-@onready var polygon_2d: Polygon2D = $StaticBody2D/CollisionPolygon2D/Polygon2D
+@export var next_level : PackedScene
 @onready var level_completed: ColorRect = $CanvasLayer/LevelCompleted
 
 func _ready() -> void:
 	RenderingServer.set_default_clear_color(Color.BLACK)
-	polygon_2d.polygon = collision_polygon_2d.polygon
 	Events.level_completed.connect(show_level_completed)
 	
 func show_level_completed():
 	level_completed.show()
+	if !next_level: 
+		# end.
+		get_tree().paused = true
+		return
+		
+	# next level.
 	get_tree().paused = true
+	await LevelTransition.fade_to_black()
+	get_tree().paused = false
+	get_tree().change_scene_to_packed(next_level)
+	LevelTransition.fade_from_black()
